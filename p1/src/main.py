f7965287f5420eac128e77a5de91873bc6af8228
zahrabashir98 <zahra_9877@yahoo.com>
2019-04-11 18:39:57 +0430

    Debug the previous code

--- p1/src/main.py
+++ p1/src/main.py
@@ -23,18 +23,16 @@ with open("../in/ha.csv") as csvfile:
 split_text = in_text.split()
 
 
-
 visited = []
-
 flag = True
 i = 0
 forms_counter = 0
-# parts_found = {}
-# parts_found[0] = {}
 parts_found = defaultdict(list) 
+total_counter = 0
 
+for j in range()
 while (flag):
-
+    total_counter += 1
     s += split_text[0][i]
 
     wc = 0
@@ -44,14 +42,16 @@ while (flag):
         if s == word and s not in visited:
             visited.append(s)
             parts_found[forms_counter].append(s)
-            
+            print(parts_found[1])
             if i == (len(split_text[0])-1):
                 # print("tahe jomle")
-
-                flag = False
-                # for i in range(forms_counter):
-                #     if parts_found[forms_counter] == parts_found[i]:
-                #         flag = False
+                # flag = False
+                print(forms_counter)
+                print(parts_found)
+                for i in range(forms_counter-1):
+                    if parts_found[forms_counter] == parts_found[i]:
+                        flag = False
+                # if parts_found[forms_counter] not in 
                 forms_counter += 1
                 s = ""
                 i = 0
@@ -59,13 +59,15 @@ while (flag):
             s = ""
 
         else:
-
+            # print("miad to else")
+            
             if i == (len(split_text[0])-1) and wc == len(persian_words):
                 
                 parts_found[forms_counter] = []
                 s = ""
                 i = -1
-
+    if total_counter > 200 :
+        flag = False
 
 
     i += 1
